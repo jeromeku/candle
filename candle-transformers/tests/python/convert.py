45fdbf6327acf3fa1f29225ddf9fa932ec5bdc11
jeromeku <jerome.ku@gmail.com>
2023-10-19 04:32:10 +0000

    add conversion script

---
+++ candle-transformers/tests/python/convert.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+import fire
+from model_checking import convert_pytorch_to_safetensors
+
+
+def main(model_id, repo_id: Optional[str] = None):
+    pass
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
