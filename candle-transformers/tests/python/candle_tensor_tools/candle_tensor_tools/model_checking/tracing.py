584222ce44ed7e9f7d6d61d4256699a8228f428d
jeromeku <jerome.ku@gmail.com>
2023-10-20 04:30:15 +0000

    add torch tracing example

--- candle-transformers/tests/python/candle_tensor_tools/candle_tensor_tools/model_checking/tracing.py
+++ candle-transformers/tests/python/candle_tensor_tools/candle_tensor_tools/model_checking/tracing.py
@@ -1,136 +1,75 @@
-from collections import OrderedDict
-
-import numpy as np
 import torch
-import torch.nn as nn
 
 
-# From https://github.com/sksq96/pytorch-summary/blob/master/torchsummary/torchsummary.py
-def summary(
-    model, input_size, batch_size=-1, device=torch.device("cuda:0"), dtypes=None
-):
-    result, params_info = summary_string(model, input_size, batch_size, device, dtypes)
-    print(result)
+def _make_module_map(model: torch.nn.Module):
+    module_map = {}
 
-    return params_info
+    for k, v in model.named_modules():
+        module_cls = v.__class__.__name__
+        if module_cls in module_map:
+            module_map[module_cls] = module_map[module_cls] + [k]
+        else:
+            module_map[module_cls] = [k]
+
+    return module_map
 
 
-def summary_string(
-    model, input_size, batch_size=-1, device=torch.device("cuda:0"), dtypes=None
-):
-    if dtypes == None:
-        dtypes = [torch.FloatTensor] * len(input_size)
+def get_module(name: str, model: torch.nn.Module):
+    """Look up module by module class name
 
-    summary_str = ""
+    Calling module.named_modules() yields weight path (str) to module mapping.  This function
+    looks up the module by *module class name* instead.
 
-    def register_hook(module):
-        def hook(module, input, output):
-            class_name = str(module.__class__).split(".")[-1].split("'")[0]
-            module_idx = len(summary)
-
-            m_key = "%s-%i" % (class_name, module_idx + 1)
-            summary[m_key] = OrderedDict()
-            summary[m_key]["input_shape"] = list(input[0].size())
-            summary[m_key]["input_shape"][0] = batch_size
-            if isinstance(output, (list, tuple)):
-                summary[m_key]["output_shape"] = [
-                    [-1] + list(o.size())[1:] for o in output
-                ]
-            else:
-                summary[m_key]["output_shape"] = list(output.size())
-                summary[m_key]["output_shape"][0] = batch_size
-
-            params = 0
-            if hasattr(module, "weight") and hasattr(module.weight, "size"):
-                params += torch.prod(torch.LongTensor(list(module.weight.size())))
-                summary[m_key]["trainable"] = module.weight.requires_grad
-            if hasattr(module, "bias") and hasattr(module.bias, "size"):
-                params += torch.prod(torch.LongTensor(list(module.bias.size())))
-            summary[m_key]["nb_params"] = params
-
-        if not isinstance(module, nn.Sequential) and not isinstance(
-            module, nn.ModuleList
-        ):
-            hooks.append(module.register_forward_hook(hook))
-
-    # multiple inputs to the network
-    if isinstance(input_size, tuple):
-        input_size = [input_size]
-
-    # batch_size of 2 for batchnorm
-    x = [
-        torch.rand(2, *in_size).type(dtype).to(device=device)
-        for in_size, dtype in zip(input_size, dtypes)
-    ]
-
-    # create properties
-    summary = OrderedDict()
-    hooks = []
-
-    # register hook
-    model.apply(register_hook)
-
-    # make a forward pass
-    # print(x.shape)
-    model(*x)
-
-    # remove these hooks
-    for h in hooks:
-        h.remove()
-
-    summary_str += (
-        "----------------------------------------------------------------" + "\n"
-    )
-    line_new = "{:>20}  {:>25} {:>15}".format("Layer (type)", "Output Shape", "Param #")
-    summary_str += line_new + "\n"
-    summary_str += (
-        "================================================================" + "\n"
-    )
-    total_params = 0
-    total_output = 0
-    trainable_params = 0
-    for layer in summary:
-        # input_shape, output_shape, trainable, nb_params
-        line_new = "{:>20}  {:>25} {:>15}".format(
-            layer,
-            str(summary[layer]["output_shape"]),
-            "{0:,}".format(summary[layer]["nb_params"]),
+    For example, given this model / module:
+        MistralForCausalLM(
+            (model): MistralModel(
+            (embed_tokens): Embedding(32000, 4096)
+            (layers): ModuleList(
+            (0-31): 32 x MistralDecoderLayer(
+                (self_attn): MistralAttention(
+                (q_proj): Linear(in_features=4096, out_features=4096, bias=False)
+                (k_proj): Linear(in_features=4096, out_features=1024, bias=False)
+                (v_proj): Linear(in_features=4096, out_features=1024, bias=False)
+                (o_proj): Linear(in_features=4096, out_features=4096, bias=False)
+                (rotary_emb): MistralRotaryEmbedding()
+                )
+                (mlp): MistralMLP(
+                (gate_proj): Linear(in_features=4096, out_features=14336, bias=False)
+                (up_proj): Linear(in_features=4096, out_features=14336, bias=False)
+                (down_proj): Linear(in_features=14336, out_features=4096, bias=False)
+                (act_fn): SiLUActivation()
+                )
+                (input_layernorm): MistralRMSNorm()
+                (post_attention_layernorm): MistralRMSNorm()
+            )
+            )
+            (norm): MistralRMSNorm()
         )
-        total_params += summary[layer]["nb_params"]
+            (lm_head): Linear(in_features=4096, out_features=32000, bias=False)
+        )
 
-        total_output += np.prod(summary[layer]["output_shape"])
-        if "trainable" in summary[layer]:
-            if summary[layer]["trainable"] == True:
-                trainable_params += summary[layer]["nb_params"]
-        summary_str += line_new + "\n"
+    Calling get_module('MistralAttention', model) will return the MistralAttention module along with its weight path.
 
-    # assume 4 bytes/number (float on cuda).
-    total_input_size = abs(
-        np.prod(sum(input_size, ())) * batch_size * 4.0 / (1024**2.0)
-    )
-    total_output_size = abs(
-        2.0 * total_output * 4.0 / (1024**2.0)
-    )  # x2 for gradients
-    total_params_size = abs(total_params * 4.0 / (1024**2.0))
-    total_size = total_params_size + total_output_size + total_input_size
+    In the case that there are multiple modules of the same class, the weight path will be a list of paths.
 
-    summary_str += (
-        "================================================================" + "\n"
-    )
-    summary_str += "Total params: {0:,}".format(total_params) + "\n"
-    summary_str += "Trainable params: {0:,}".format(trainable_params) + "\n"
-    summary_str += (
-        "Non-trainable params: {0:,}".format(total_params - trainable_params) + "\n"
-    )
-    summary_str += (
-        "----------------------------------------------------------------" + "\n"
-    )
-    summary_str += "Input size (MB): %0.2f" % total_input_size + "\n"
-    summary_str += "Forward/backward pass size (MB): %0.2f" % total_output_size + "\n"
-    summary_str += "Params size (MB): %0.2f" % total_params_size + "\n"
-    summary_str += "Estimated Total Size (MB): %0.2f" % total_size + "\n"
-    summary_str += (
-        "----------------------------------------------------------------" + "\n"
-    )
-    # return summary
-    return summary, summary_str, (total_params, trainable_params)
+    Args:
+        name (str): module class name
+        model (torch.nn.Module): model to search
+
+    Returns:
+        path (list[str]): weight path to module
+        module (torch.nn.Module): module
+    """
+
+    module_map = _make_module_map(model)
+    modules = dict(model.named_modules())
+
+    path = module_map[name]
+
+    key = path[0]
+    fmt_path = path[0] + ", ... ," + path[-1] if len(path) > 1 else path[0]
+
+    module = modules.get(key)
+    print(f"{fmt_path}: {module}")
+
+    return path, module
